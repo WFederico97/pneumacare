20a66c10ab01e720729d3f67a17437ca1ada56cf
WFederico97 <wuthrich09@gmail.com>
2026-06-06 17:38:11 -0300

    feat(shared): add new exception message

--- src/main/java/wfederico/pneumacare/shared/constants/ExceptionMessageConstants.java
+++ src/main/java/wfederico/pneumacare/shared/constants/ExceptionMessageConstants.java
@@ -10,4 +10,6 @@ public final class ExceptionMessageConstants {
 
     /* Error message when pplat is <= peepTotal in calculateCstat */
     public static final String CSTAT_FORMULA_ERROR = "La presión meseta debe ser mayor que el PEEP total";
+    /* Error message when the ventilator brand is not found */
+    public static final String UNKNOWN_BRAND_ERROR = "Marca de ventilador desconocida: ";
 }
