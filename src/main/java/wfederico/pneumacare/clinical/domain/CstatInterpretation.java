d78b37ccab43b833b80c511bd06c4f22e203ef3a
WFederico97 <wuthrich09@gmail.com>
2026-06-06 17:39:01 -0300

    feat(domain): add new records

--- src/main/java/wfederico/pneumacare/clinical/domain/CstatInterpretation.java
+++ src/main/java/wfederico/pneumacare/clinical/domain/CstatInterpretation.java
@@ -3,5 +3,14 @@ package wfederico.pneumacare.clinical.domain;
 public enum CstatInterpretation {
     HIGH,
     NORMAL,
-    LOW
+    LOW;
+
+    private static final double HIGH_LOWER = 100.0;
+    private static final double NORMAL_LOWER = 50.0;
+
+    public static CstatInterpretation from(double cstat){
+        if (cstat >= HIGH_LOWER) return HIGH;
+        if (cstat >= NORMAL_LOWER) return NORMAL;
+        return LOW;
+    };
 }
--- src/main/java/wfederico/pneumacare/clinical/domain/PafiClassification.java
+++ src/main/java/wfederico/pneumacare/clinical/domain/PafiClassification.java
@@ -5,5 +5,18 @@ public enum PafiClassification {
     AT_RISK,
     MILD_ARDS,
     MODERATE_ARDS,
-    SEVERE_ARDS
+    SEVERE_ARDS;
+
+    private static final double NORMAL_LOWER = 400.0;
+    private static final double AT_RISK_LOWER = 300.0;
+    private static final double MILD_ARDS_LOWER = 200.0;
+    private static final double MODERATE_ARDS_LOWER = 100.0;
+
+    public static PafiClassification from(double pafi){
+        if (pafi >= NORMAL_LOWER) return NORMAL;
+        if (pafi >= AT_RISK_LOWER) return AT_RISK;
+        if (pafi >= MILD_ARDS_LOWER) return MILD_ARDS;
+        if (pafi >= MODERATE_ARDS_LOWER) return MODERATE_ARDS;
+        return  SEVERE_ARDS;
+    }
 }
