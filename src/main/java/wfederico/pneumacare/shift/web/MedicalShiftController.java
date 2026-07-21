018fcaf7a84b94499a37c6217413cc9660490bad
WFederico97 <wuthrich09@gmail.com>
2026-07-21 00:35:07 -0300

    chore: remove dead code (ai_clinical_insights table, BackendServiceCard, stray import)

--- src/main/java/wfederico/pneumacare/shift/web/MedicalShiftController.java
+++ src/main/java/wfederico/pneumacare/shift/web/MedicalShiftController.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.coyote.Response;
 import org.slf4j.MDC;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
